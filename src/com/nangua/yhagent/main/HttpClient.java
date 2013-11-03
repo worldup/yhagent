9f01737ac0db62055cb26c7d8141f64fd4cd0c1e
worldup <worldup@163.com>
2013-11-03 17:02:00 +0800

    add doc

--- src/com/nangua/yhagent/main/HttpClient.java
+++ src/com/nangua/yhagent/main/HttpClient.java
@@ -1,5 +1,6 @@
 package com.nangua.yhagent.main;
 
+import com.nangua.yhagent.bean.ChgRoomBean;
 import com.nangua.yhagent.bean.RegisterRoomBean;
 import com.nangua.yhagent.bean.UnRegisterRoomBean;
 import com.nangua.yhagent.command.Invoker;
@@ -9,20 +10,28 @@ public class HttpClient {
 
 		YHChannelFactory channelFactory = YHChannelFactory.newInstance();
 		Invoker invoker = Invoker.newInstance(channelFactory);
-		
-		  UnRegisterRoomBean uBean = new UnRegisterRoomBean();
+		  UnRegisterRoomBean uBean1 = new UnRegisterRoomBean();
+			uBean1.setRoomNo("001");
+		invoker.unRegisterRoom(uBean1);  
+	   UnRegisterRoomBean uBean = new UnRegisterRoomBean();
 		uBean.setRoomNo("002");
-		invoker.unRegisterRoom(uBean); 
-		RegisterRoomBean bean = new RegisterRoomBean();
+		invoker.unRegisterRoom(uBean);  
+		 RegisterRoomBean bean = new RegisterRoomBean();
 		bean.setRoomNo("002").setGuestName("邓鑫").setPurview("101").setSex("0")
 				.setAddress("上海市闵行区XX路XX弄").setEmail("abcdedf@163.com")
 				.setPhone("13888888888").setCBillPolicy("0").setSzRegDate(
 						"2013:03:31 12:03:00").setCCanViewRecord("1");
-		invoker.registerRoom(bean);
-
-       /*
+		invoker.registerRoom(bean); 
+ 
+		 RegisterRoomBean bean2 = new RegisterRoomBean();
+			bean2.setRoomNo("002").setGuestName("邓鑫").setPurview("101").setSex("0")
+					.setAddress("上海市闵行区XX路XX弄").setEmail("abcdedf@163.com")
+					.setPhone("13888888888").setCBillPolicy("0").setSzRegDate(
+							"2013:03:31 12:03:00").setCCanViewRecord("1");
+			invoker.registerRoom(bean2); 
 		ChgRoomBean cBean = new ChgRoomBean();
-		cBean.setPreRoomNo("002").setNewRoomNo("001");*/
+		cBean.setPreRoomNo("002").setNewRoomNo("001"); 
+		invoker.chgRoom(cBean);
 		channelFactory.destroy();
 
 	}
